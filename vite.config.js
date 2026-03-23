f192b8e6cb7916cc234952a9209103e6c623ba32
pejalo <6892407+pejalo@users.noreply.github.com>
2026-03-23 21:38:55 +0700

    add dist folder to host on github

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   base: './', // This ensures assets are linked relatively
+  build: {
+    outDir: 'docs', // required to deploy on gh-pages
+  }
 })
